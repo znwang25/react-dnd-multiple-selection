1227452281ce6afdc70e6055d3a2bf6fd3e2ffeb
znwang25 <znwang25@gmail.com>
2020-12-06 15:00:19 -0800

    Updates

---
+++ precache-manifest.1b84a65ca8034bcafae2e901fc7d88c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee75820241eccccc33ae0008eb769390",
+    "url": "/react-dnd-multiple-selection/index.html"
+  },
+  {
+    "revision": "b5db9db8de83e76603ed",
+    "url": "/react-dnd-multiple-selection/static/css/main.fe7f23ef.chunk.css"
+  },
+  {
+    "revision": "76130f2142b1332edab2",
+    "url": "/react-dnd-multiple-selection/static/js/2.3f1f85cc.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/react-dnd-multiple-selection/static/js/2.3f1f85cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5db9db8de83e76603ed",
+    "url": "/react-dnd-multiple-selection/static/js/main.5f853885.chunk.js"
+  },
+  {
+    "revision": "a1b19e636c71ebd8d57f",
+    "url": "/react-dnd-multiple-selection/static/js/runtime-main.4ad74e5a.js"
+  }
+]);
